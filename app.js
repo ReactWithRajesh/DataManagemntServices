42691d9fbd31e701bee4994327888f5492f8be63
Rajesh <rksmech79@gmail.com>
2023-12-29 03:21:51 +0530

    updadted

--- app.js
+++ app.js
@@ -1,52 +1,49 @@
-require('./models/db')
-const jwt = require('jsonwebtoken')
-const env = require('dotenv')
+require('./models/db');
+const jwt = require('jsonwebtoken');
+const env = require('dotenv');
 const cors = require('cors');
-env.config()
-const port = process.env.PORT || 4041
+env.config();
+
+const port = process.env.PORT || 4041;
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
 const path = require('path');
 const handlebars = require('handlebars');
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 
-const studentController = require('./controllers/studentController')
-const loginController = require('./controllers/loginController')
-const bookingController = require('./controllers/bookingController')
+const studentController = require('./controllers/studentController');
+const loginController = require('./controllers/loginController');
+const bookingController = require('./controllers/bookingController');
 
-const app = express()
+const app = express();
 
-//midlewares 
-app.use(morgan('dev'))
-app.options('*', cors())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use('/user', loginController)
-app.use('/student', studentController)
-app.use('/item', bookingController)
+// Middleware
+app.use(morgan('dev'));
+app.use(cors()); // Enable CORS for all routes
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// app.get('/', (req, res) => {
-//     res.render('./dashboard.hbs');
-// });
+// Routes
+app.use('/user', loginController);
+app.use('/student', studentController);
+app.use('/item', bookingController);
+
+// Serve HTML file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './dashboard.html'));
 });
 
+// Configure Handlebars
 app.set("views", path.join(__dirname, "/views/"));
+app.engine("hbs", exphbs({
+    handlebars: allowInsecurePrototypeAccess(handlebars),
+    extname: 'hbs',
+    defaultLayout: 'MainLayout',
+    layoutsDir: path.join(__dirname, '/views/layouts/')
+}));
+app.set('view engine', 'hbs');
 
-app.engine("hbs",
-    exphbs({
-        handlebars: allowInsecurePrototypeAccess(handlebars),
-        extname: 'hbs',
-        defaultLayout: 'MainLayout',
-        layoutsDir: path.join(__dirname,'/views/layouts/') 
-    })
-);
-
-
-
-app.set('view engine', 'hbs')
-
-app.listen(port, (req, res) => console.log(`Server is running at port : ${port}`))
+// Start the server
+app.listen(port, () => console.log(`Server is running at port: ${port}`));
